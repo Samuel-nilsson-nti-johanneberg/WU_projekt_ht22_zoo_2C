fcac9a5798a1344a99b02d12f064b3d5a43a1781
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2022-12-13 00:02:06 +0100

    jobbat lite

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ $(function() {
       return false;
     });
   });
+
+
+//   $(function() {
+//     $('a[href*=#]').on('click', function(e) {
+//       e.preventDefault();
+//       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//     });
+//   });
